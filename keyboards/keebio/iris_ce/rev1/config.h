e1e0a2e9ad4f408a21741bdcdfd99fdfef271aa8
Timothy Ng <timnjj@gmail.com>
2024-10-05 09:59:16 -0700

    set OSK timeout to 1.5 seconds

--- keyboards/keebio/iris_ce/rev1/config.h
+++ keyboards/keebio/iris_ce/rev1/config.h
@@ -20,3 +20,4 @@
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM_PER_KEY // Defaults to TAPPING_TERM if not set
 #define ONESHOT_TAP_TOGGLE 2 // Double tap OSMs to lock. This is affected by the quick tap term.
+#define ONESHOT_TIMEOUT 1500  // Time (in ms) before the one shot key is released
